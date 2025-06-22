ef0154e348a09ed3f59eafe5fa651b4c18f84333
Pratik-karki07 <studypratikk@gmail.com>
2025-06-22 21:39:25 +0545

    Add new Components and Files

---
+++ Day4(proptypes and Defaultprops)/App.jsx
@@ -0,0 +1,18 @@
+import Student from './Student.jsx'
+
+function App() {
+  return(
+    <>
+      <Student name="Aayush" age="20" isStudent={true}/>
+      <Student name="Ajay" age={23} isStudent={true}/>
+      <Student name="Aashish" age={26} isStudent={false}/>
+      <Student/>
+      
+    </>
+
+  );
+  
+  
+}
+
+export default App
